4f66b612507b83333dc2450cbae56e7cc88de9ae
Martin Chapman <contact@martinchapman.co.uk>
2023-08-12 14:51:09 +0100

    Update made by Phenoflow

---
+++ python/asthma-secondary-care-unspecified---secondary.py
@@ -0,0 +1,25 @@
+# Luke Daines, Ann Morgan, Mome Mukherjee, Mohammad Al Sallakh, Eimear O'Rourke, Jennifer K Quint, 2023.
+
+import sys, csv, re
+
+codes = [{"code":"J45.901","system":"icd10"},{"code":"J45.909","system":"icd10"},{"code":"J45.901","system":"icd10"},{"code":"J45.909","system":"icd10"},{"code":"493.9","system":"icd9"},{"code":"493.90","system":"icd9"},{"code":"493.91","system":"icd9"},{"code":"493.9","system":"icd9"},{"code":"493.90","system":"icd9"},{"code":"493.91","system":"icd9"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('asthma-secondary-care-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["asthma-secondary-care-unspecified---secondary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["asthma-secondary-care-unspecified---secondary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["asthma-secondary-care-unspecified---secondary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
